d99ff523ff61629aa072b18fbbf7f965c188ab8d
Andrew Tyustin <andrei.tyustin@mail.ru>
2022-11-16 22:34:25 +0300

    ПР-4 правки-1

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
 const editProfile = document.querySelector('.profile__editor');
-const popupEditProfile = document.querySelector('.popup-edit');
-const popupCloseIcon = popupEditProfile.querySelector('.popup-edit__close');
+const popupEditProfile = document.querySelector('.popup');
+const popupCloseIcon = popupEditProfile.querySelector('.popup__close');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const nameInput = popupEditProfile.querySelector('.popup-edit__input_item_name');
-const descriptionInput = popupEditProfile.querySelector('.popup-edit__input_item_description');
+const nameInput = popupEditProfile.querySelector('.popup__input_item_name');
+const descriptionInput = popupEditProfile.querySelector('.popup__input_item_description');
 const popupOpen = function () {
-  popupEditProfile.classList.add('popup-edit_opened');
+  popupEditProfile.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
 }
 const popupClose = function () {
-  popupEditProfile.classList.remove('popup-edit_opened');
+  popupEditProfile.classList.remove('popup_opened');
 }
 const formSubmitHandler = function (evt) {
   evt.preventDefault();
