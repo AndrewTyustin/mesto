a9dcdd2c1a4cb44e9defd77aface0254877cd96d
Andrew Tyustin <andrei.tyustin@mail.ru>
2022-11-29 23:56:13 +0300

    ПР-5 fix-1

--- scripts/index.js
+++ scripts/index.js
@@ -30,6 +30,8 @@ const addCardIcon = document.querySelector('.profile__add-mesto');
 const popupProfile = document.querySelector('#profile-popup');
 const popupCards = document.querySelector('#cards-popup');
 const popupImageZoom = document.querySelector('#image-popup');
+const popupImageZoomDescription = popupImageZoom.querySelector('.popup__description');
+const popupImageZoomImage = popupImageZoom.querySelector('.popup__image');
 const popupProfileCloseIcon = popupProfile.querySelector('.popup__close');
 const popupCardsCloseIcon = popupCards.querySelector('.popup__close');
 const popupImageZoomCloseIcon = popupImageZoom.querySelector('.popup__close');
@@ -40,22 +42,33 @@ const nameCardInput = popupCards.querySelector('.popup__input_item_name');
 const descriptionInput = popupProfile.querySelector('.popup__input_item_description');
 const linkCardInput = popupCards.querySelector('.popup__input_item_description');
 const cardsArea = document.querySelector('.cards');
+const closeButtons = document.querySelectorAll('.popup__close');
 
-const popupOpen = function (popupName) {
+const openPopup = function (popupName) {
   popupName.classList.add('popup_opened');
+}
+
+const openPopupProfile = function () {
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
+  openPopup(popupProfile);
 }
-const popupClose = function (popupName) {
+
+const closePopup = function (popupName) {
   popupName.classList.remove('popup_opened');
 }
+
 
 const addCards = function (name, link) {
   const contentCardTemplate = document.querySelector('#card-template').content;
   const copyCardTemplate = contentCardTemplate.querySelector('.cards__item').cloneNode(true);
 
-  copyCardTemplate.querySelector('.cards__description').textContent = name;
-  copyCardTemplate.querySelector('.cards__image').src = link;
+  const cardsImage = copyCardTemplate.querySelector('.cards__image');
+  const cardDescription = copyCardTemplate.querySelector('.cards__description');
+
+  cardDescription.textContent = name;
+  cardsImage.src = link;
+  cardsImage.alt = name;
 
   copyCardTemplate.querySelector('.cards__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('cards__like_active');
@@ -66,40 +79,43 @@ const addCards = function (name, link) {
   });
 
   const getZoomImages = function () {
-    popupImageZoom.querySelector('.popup__description').textContent = name;
-    popupImageZoom.querySelector('.popup__image').src = link;
-    popupOpen(popupImageZoom);
+    popupImageZoomDescription.textContent = name;
+    popupImageZoomImage.src = link;
+    popupImageZoomImage.alt = name;
+    openPopup(popupImageZoom);
   }
 
-  copyCardTemplate.querySelector('.cards__image').addEventListener('click', getZoomImages);
+  cardsImage.addEventListener('click', getZoomImages);
 
   return copyCardTemplate;
 }
 
-const integrationCard = function (evt) {
+const addNewCard = function (evt) {
   evt.preventDefault();
   cardsArea.prepend(addCards(nameCardInput.value, linkCardInput.value));
-  popupClose(popupCards);
+  evt.target.reset();
+  closePopup(popupCards);
 }
-const integrationInitialCards = function () {
+const renderInitialCards = function () {
   initialCards.forEach(function (card) {
     cardsArea.append(addCards(card.name, card.link));
   });
 }
 
-integrationInitialCards();
+renderInitialCards();
 
-const formSubmitHandler = function (evt) {
+const handleProfileFormSubmit = function (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
-  popupClose(popupProfile);
+  closePopup(popupProfile);
 }
 
-editProfileIcon.addEventListener('click', () => popupOpen(popupProfile));
-addCardIcon.addEventListener('click', () => popupOpen(popupCards));
-popupProfileCloseIcon.addEventListener('click', () => popupClose(popupProfile));
-popupCardsCloseIcon.addEventListener('click', () => popupClose(popupCards));
-popupImageZoomCloseIcon.addEventListener('click', () => popupClose(popupImageZoom));
-popupProfile.addEventListener('submit', formSubmitHandler);
-popupCards.addEventListener('submit', integrationCard);
+editProfileIcon.addEventListener('click', () => openPopup(popupProfile));
+addCardIcon.addEventListener('click', () => openPopup(popupCards));
+closeButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
+popupProfile.addEventListener('submit', handleProfileFormSubmit);
+popupCards.addEventListener('submit', addNewCard);
